b6a2a4bf9248011143308714e2cc62b1472bd008
Adrian <ulmeanu.a@gmail.com>
2025-05-28 20:13:14 +0300

    changes multiple

--- nodes/Wamm.node.ts
+++ nodes/Wamm.node.ts
@@ -18,7 +18,7 @@ export class Wamm implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'WAMM PRO',
 		name: 'wammpro',
-		icon: 'whatsapp',
+		icon: 'file:wamm.png',
 		group: ['communication'],
 		version: 1,
 		subtitle: 'Send WhatsApp messages via WAMM API',
